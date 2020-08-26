c7e74b762f65d857fe4f7fd32a8b45f5a39710a9
Jlobblet <jlobblet@gmx.com>
2020-08-26 17:48:48 +0100

    Use auto properties

--- Barotrauma-Circuit-Resolver/Util/GraphParts.cs
+++ Barotrauma-Circuit-Resolver/Util/GraphParts.cs
@@ -7,11 +7,6 @@ namespace Barotrauma_Circuit_Resolver.Util
 {
     public class Vertex
     {
-        private int id;
-        private string name;
-        private HashSet<Edge<Vertex>> outgoingEdges;
-        private HashSet<Edge<Vertex>> incomingEdges;
-
         public Vertex(int id)
         {
             new Vertex(id, id.ToString());
@@ -25,11 +20,15 @@ namespace Barotrauma_Circuit_Resolver.Util
             IncomingEdges = new HashSet<Edge<Vertex>>();
         }
 
-        public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
         public int Order => Edges.Count();
-        internal HashSet<Edge<Vertex>> OutgoingEdges { get => outgoingEdges; set => outgoingEdges = value; }
-        internal HashSet<Edge<Vertex>> IncomingEdges { get => incomingEdges; set => incomingEdges = value; }
+        internal HashSet<Edge<Vertex>> OutgoingEdges { get; set; }
+
+        internal HashSet<Edge<Vertex>> IncomingEdges { get; set; }
+
         internal HashSet<Edge<Vertex>> Edges => OutgoingEdges.Union(IncomingEdges).ToHashSet();
 
         public override bool Equals(object obj)
@@ -67,18 +66,15 @@ namespace Barotrauma_Circuit_Resolver.Util
 
     public class Edge<TVertex> : IEdge<TVertex>
     {
-        private readonly TVertex source;
-        private readonly TVertex target;
-
         public Edge(TVertex source, TVertex target)
         {
-            this.source = source;
-            this.target = target;
+            Source = source;
+            Target = target;
         }
 
-        public TVertex Source => source;
+        public TVertex Source { get; }
 
-        public TVertex Target => target;
+        public TVertex Target { get; }
 
         public string Name => ToString();
 
