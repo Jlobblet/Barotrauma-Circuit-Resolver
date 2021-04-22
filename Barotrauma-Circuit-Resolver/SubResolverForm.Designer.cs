4b889763f4a54f192a0c89df39b48566316ae509
WorkingJoe <a.w.j.rietveld@student.tudelft.nl>
2021-04-22 11:48:10 +0200

    Updated window scaling

--- Barotrauma-Circuit-Resolver/SubResolverForm.Designer.cs
+++ Barotrauma-Circuit-Resolver/SubResolverForm.Designer.cs
@@ -48,13 +48,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.FilepathTextBox.Location = new System.Drawing.Point(10, 11);
             this.FilepathTextBox.Name = "FilepathTextBox";
-            this.FilepathTextBox.Size = new System.Drawing.Size(337, 23);
+            this.FilepathTextBox.Size = new System.Drawing.Size(387, 23);
             this.FilepathTextBox.TabIndex = 0;
             // 
             // BrowseButton
             // 
             this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.BrowseButton.Location = new System.Drawing.Point(353, 11);
+            this.BrowseButton.Location = new System.Drawing.Point(403, 11);
             this.BrowseButton.Name = "BrowseButton";
             this.BrowseButton.Size = new System.Drawing.Size(61, 21);
             this.BrowseButton.TabIndex = 1;
@@ -65,9 +65,9 @@
             // GoButton
             // 
             this.GoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.GoButton.Location = new System.Drawing.Point(249, 213);
+            this.GoButton.Location = new System.Drawing.Point(299, 335);
             this.GoButton.Name = "GoButton";
-            this.GoButton.Size = new System.Drawing.Size(165, 75);
+            this.GoButton.Size = new System.Drawing.Size(165, 94);
             this.GoButton.TabIndex = 2;
             this.GoButton.Text = "Go";
             this.GoButton.UseVisualStyleBackColor = true;
@@ -78,10 +78,11 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 37);
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 39);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(403, 143);
+            this.pictureBox1.Size = new System.Drawing.Size(453, 260);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -92,11 +93,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.SettingGroupBox.Controls.Add(this.SaveGraphCheckBox);
             this.SettingGroupBox.Controls.Add(this.NewSubCheckBox);
-            this.SettingGroupBox.Location = new System.Drawing.Point(10, 213);
+            this.SettingGroupBox.Location = new System.Drawing.Point(10, 335);
             this.SettingGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SettingGroupBox.Name = "SettingGroupBox";
             this.SettingGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.SettingGroupBox.Size = new System.Drawing.Size(221, 71);
+            this.SettingGroupBox.Size = new System.Drawing.Size(271, 94);
             this.SettingGroupBox.TabIndex = 4;
             this.SettingGroupBox.TabStop = false;
             this.SettingGroupBox.Text = "Settings";
@@ -127,9 +128,9 @@
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(9, 185);
+            this.progressBar1.Location = new System.Drawing.Point(10, 304);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(404, 23);
+            this.progressBar1.Size = new System.Drawing.Size(454, 23);
             this.progressBar1.TabIndex = 5;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
@@ -139,7 +140,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.label1.Location = new System.Drawing.Point(13, 189);
+            this.label1.Location = new System.Drawing.Point(13, 330);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 15);
             this.label1.TabIndex = 6;
@@ -148,7 +149,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 295);
+            this.ClientSize = new System.Drawing.Size(475, 436);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.SettingGroupBox);
@@ -157,7 +158,8 @@
             this.Controls.Add(this.BrowseButton);
             this.Controls.Add(this.FilepathTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(441, 334);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(450, 215);
             this.Name = "SubResolverForm";
             this.Text = "Barotrauma Circuit Resolver";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
